76077605b2484330fe444049a939a66240716c71
saif <saifps2006@gmail.com>
2026-01-24 21:31:47 +0300

    idisposable pattern

---
+++ AdvancedProgramming/IDisposablePattern.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace AdvancedProgramming;
+/*
+ 
+Use IDisposable when your class owns a resource that must be released explicitly, especially:
+
+    Unmanaged resources
+
+    Files
+
+    Sockets
+
+    Database connections
+
+    OS handles
+
+Managed objects that implement IDisposable
+
+    FileStream
+
+    HttpClient
+
+    SqlConnection
+
+    Socket
+
+ */
+
+class CurrencyService : IDisposable
+{
+
+    bool _disposed;
+    HttpClient _httpClient;
+
+    public CurrencyService()
+    {
+        _httpClient = new HttpClient();
+    }
+
+    // true : dispose managed code + unmanaged code
+    // false : dispose unmanaged code only
+    protected virtual void Dispose( bool disposing )
+    {
+        if (_disposed)
+            return;
+        
+        if ( disposing)
+        {
+            _httpClient?.Dispose();
+        }
+
+        // unmanaged code
+        // in this example there is not unmanaged code
+
+        _disposed = true;
+
+
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize( this ); // you dont have to call the finilizer
+    }
+    
+    // if the user forgot to call the dispose method
+    ~CurrencyService() { 
+        Dispose(false);
+    }
+
+    public string GetCurrencies ()
+    {
+        return _httpClient.GetStringAsync("https://coinbase.com/api/v2/currencies").Result;
+    }
+
+}
+
--- AdvancedProgramming/Program.cs
+++ AdvancedProgramming/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+
+//using System.Security;
+//using AdvancedProgramming;
+
+//using ( CurrencyService currencyService = new CurrencyService() )
+//{
+
+//    Console.Write(currencyService.GetCurrencies());
+//}
+
+HttpClient _httpClient = new HttpClient();
+_httpClient.Dispose();
+//var result = _httpClient.GetStringAsync("https://coinbase.com/api/v2/currencies").Result;
+Console.WriteLine("result");
+

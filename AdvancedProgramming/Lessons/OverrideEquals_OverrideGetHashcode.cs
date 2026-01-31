d4b9b81a270c5904873c119e2601ac9c80668c3c
saif <saifps2006@gmail.com>
2026-01-31 22:58:06 +0300

    implementing IEquatable

--- AdvancedProgramming/Lessons/OverrideEquals_OverrideGetHashcode.cs
+++ AdvancedProgramming/Lessons/OverrideEquals_OverrideGetHashcode.cs
@@ -9,13 +9,13 @@ public static class OverrideEquals_OverrideGetHashcode
     /// Equal the .GetHashCode() will return a reference based integer
     /// which will make dealing with this object is broken
     /// </summary>
-    class Person 
+    class Person_1 
     {
         public int Id { get; set; }
         public string Name { get; set; }
 
 
-        public Person(int Id , string Name)
+        public Person_1(int Id , string Name)
         {
             this.Name = Name;
             this.Id = Id;
@@ -23,7 +23,7 @@ public static class OverrideEquals_OverrideGetHashcode
 
         public override bool Equals(object? obj)
         {
-            Person otherPerson = obj as Person; // if type casting is not compitable it returns null
+            Person_1 otherPerson = obj as Person_1; // if type casting is not compitable it returns null
 
             if ( otherPerson is null) return false;
 
@@ -53,6 +53,8 @@ public static class OverrideEquals_OverrideGetHashcode
 
         }
 
+        // fields that take place in the equals method impelementation
+        // should take place in GetHashCode only ( no less or more )
         public override int GetHashCode()
         {
             // this is a hash function based of multiple parameters
@@ -61,6 +63,44 @@ public static class OverrideEquals_OverrideGetHashcode
 
 
     }
+
+    // using the IEquatable interface
+
+    class Person_3 : IEquatable<Person_3>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Person_3(string Name, int Id)
+        {
+            this.Name = Name;
+            this.Id = Id;
+        }
+        public override bool Equals(object? obj)
+        {
+            Person_2? otherPerson = obj as Person_2; // if type casting is not compitable it returns null
+
+            return Equals(otherPerson);
+
+        }
+        public bool Equals(Person_3? other)
+        {
+            if (other is null) return false;
+
+            return other.Id == this.Id && other.Name == this.Name;
+        }
+
+        // fields that take place in the equals method impelementation
+        // should take place in GetHashCode only ( no less or more )
+        public override int GetHashCode()
+        {
+            // this is a hash function based of multiple parameters
+            return HashCode.Combine(this.Id, this.Name);
+        }
+
+    }
+
+
 
 
 }
--- AdvancedProgramming/Program.cs
+++ AdvancedProgramming/Program.cs
@@ -1,37 +1,144 @@
 ﻿
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Metrics;
 using System.Numerics;
 using System.Text;
 using AdvancedProgramming.Lessons;
 
 class program
 {
+    
     class Person
     {
-        public string? Name;
-        public string? Address;
+        public int age;
+        public int tall;
+        public int weight;
+        public string Name;
+        
+        public Person(string Name , int age, int tall , int weight)
+        {
+            this.Name = Name;
+            this.age = age;
+            this.tall = tall;
+            this.weight = weight;
+        }
+        public override bool Equals(object? obj)
+        {
+            Person otherPerson = obj as Person;
+            if (otherPerson == null) return false;
+
+            return otherPerson.Name == this.Name && otherPerson.age == this.age && this.tall == otherPerson.tall && this.weight == otherPerson.weight;
+        }
+        public override string ToString()
+        {
+            return $"Name : {Name} Age : {age} Tall : {tall} Weight : {weight}";
+        }
     }
-    struct car
+
+    class Person2 : IEquatable<Person2>
     {
-        public int wheels;
-        public int body;
-        public Person driver;
+        public int age;
+        public int tall;
+        public int weight;
+        public string Name;
+
+        public Person2(string Name, int age, int tall, int weight)
+        {
+            this.Name = Name;
+            this.age = age;
+            this.tall = tall;
+            this.weight = weight;
+        }
+        public override bool Equals(object? obj)
+        {
+            if  (obj == null) return false;
+            return Equals((Person2)obj);
+        }
+
+        public bool Equals(Person2 other)
+        {
+            return other.Name == this.Name && other.age == this.age && this.tall == other.tall && this.weight == other.weight;
+        }
+
+        public override string ToString()
+        {
+            return $"Name : {Name} Age : {age} Tall : {tall} Weight : {weight}";
+        }
+    }
+
+    public static void IEquatableNotImplemented(long repititions = 10_000_000)
+    {
+        List<Person> people = new List<Person>();
+
+        Person personToCompare = new Person("saif", 17, 177, 34);
+
+        string[] names = { "saif", "tala", "sara" };
+        Random rand = new Random();
+        for (int i = 0; i < repititions; i++)
+        {
+            string randomName = names[rand.Next(0, names.Length)];
+            int randomAge = rand.Next(0, 19);
+            int randomWeight = rand.Next(20, 40);
+            int randomTall = rand.Next(150, 181);
+            people.Add(new Person(randomName, randomAge, randomTall, randomWeight));
+        }
+
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+        int counter = 0;
+        foreach (var p in people)
+        {
+            if (p.Equals(personToCompare))
+                counter++;
+        }
+        stopwatch.Stop();
+
+        Console.WriteLine(stopwatch.ElapsedMilliseconds / 1000m + " seconds " + counter + " match found");
+        
+
+
+    }
+    
+    public static void IEquatableImplemented(long repitions = 10_000_000)
+    {
+        List<Person2> people = new List<Person2>();
+
+        Person2 personToCompare = new Person2("saif", 17, 177, 34);
+
+        string[] names = { "saif", "tala", "sara" };
+        Random rand = new Random();
+        for (int i = 0; i < repitions; i++)
+        {
+            string randomName = names[rand.Next(0, names.Length)];
+            int randomAge = rand.Next(0, 19);
+            int randomWeight = rand.Next(20, 40);
+            int randomTall = rand.Next(150, 181);
+            people.Add(new Person2(randomName, randomAge, randomTall, randomWeight));
+        }
+
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+        int counter = 0;
+        foreach (var p in people)
+        {
+            if (p.Equals(personToCompare))
+                counter++;
+        }
+        stopwatch.Stop();
+
+        Console.WriteLine(stopwatch.ElapsedMilliseconds / 1000m + " seconds " + counter + " match found");
+
+
     }
 
     public static void Main ()
     {
-        
-        Person person = new Person ();
 
-        person.Name = "saif";
-        person.Address = "123 street";
 
-        Console.WriteLine(person.GetHashCode());
+        IEquatableImplemented(100_000_000);
 
-        Person person2 = new Person ();
-        person2.Name = "khaled";
-        person2.Address = "123 shallow street";
-
-        Console.WriteLine(person2.GetHashCode());
+        IEquatableNotImplemented(100_000_000);
 
 
     }
